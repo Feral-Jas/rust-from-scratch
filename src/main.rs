cd2fa0bbc82f4f611dc7cf90c88d9769e5d14d46
JamesLiu <906028518@qq.com>
2021-04-26 12:36:31 +0800

    u32 type & String

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
     println!("start a guess");
+
+    let secret = rand::thread_rng().gen_range(1, 100);
+
+    println!("generated number");
 
     let mut guess = String::new();
 
@@ -10,6 +16,12 @@ fn main() {
         .expect("you are not guessing a right word");
     //The right way to suppress the warning is to actually write error handling in Chapter 9
 
+    let guess: u32 = guess.trim().parse().expect("plz type a number");
     println!("your guess:{}", guess);
     //brackets are placeholders for variables
+    match guess.cmp(&secret) {
+        Ordering::Less => println!("Too small"),
+        Ordering::Equal => println!("fit!"),
+        Ordering::Greater => println!("Too big"),
+    }
 }
